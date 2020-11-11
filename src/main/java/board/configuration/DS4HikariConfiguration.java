ed89deb8fcd79903914f2c5ac4d0b7610b9c419c
oryong2 <oryong2@EA-CHOI02.tsb.co.kr>
2020-11-11 10:01:54 +0900

    Add to local repository

---
+++ src/main/java/board/configuration/DS4HikariConfiguration.java
@@ -0,0 +1,34 @@
+package board.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+//application.properties 를 사용할 수 있도록 위치 지정
+//@PropertySource 추가하여 다른 설정 파일도 사용 가능
+@PropertySource("classpath:/application.properties")
+public class DS4HikariConfiguration {
+	
+	@Bean
+	//application.properties파일의 spring.datasource.hikari prefix로 되어 있는 설정 사용
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+	
+	@Bean
+	//위의 Hikari CP 설정 이용하여 DS 생성
+	public DataSource dataSource() throws Exception {
+		DataSource dataSource = new HikariDataSource(this.hikariConfig());
+		System.out.println(dataSource.toString());
+		return dataSource;
+	}
+
+}
