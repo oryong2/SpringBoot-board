7a684a4e29f610bf7ba5680cfcb8a76d114e13be
oryong2 <oryong2@EA-CHOI02.tsb.co.kr>
2020-11-16 09:35:13 +0900

    file upload/download 설정

--- src/main/java/board/configuration/WebMvcConfiguration.java
+++ src/main/java/board/configuration/WebMvcConfiguration.java
@@ -1,6 +1,8 @@
 package board.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +15,17 @@ public class WebMvcConfiguration implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new ReqStartEndLoggingInterceptor());
 	}
+	
+	//파일 Up/Download를 위한 Apache Common Fileupload 구현체로 MultipartResolver 등록
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonMultipartResolver = new CommonsMultipartResolver();
+		commonMultipartResolver.setDefaultEncoding("UTF-8");
+		//Upload 파일 크기 제한 5MB
+		commonMultipartResolver.setMaxUploadSize(5*1024*1024);
+		return commonMultipartResolver;
+	}
+	
 
 		
 
