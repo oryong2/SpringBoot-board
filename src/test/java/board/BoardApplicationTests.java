70a6be0c93ec0c46e15edbf224dbba2c31a29522
oryong2 <oryong2@EA-CHOI02.tsb.co.kr>
2020-11-21 21:02:57 +0900

    test compile remove

--- src/test/java/board/BoardApplicationTests.java
+++ src/test/java/board/BoardApplicationTests.java
@@ -1,24 +1,9 @@
 package board;
 
-import org.junit.jupiter.api.Test;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class BoardApplicationTests {
-
-	@Autowired
-	private SqlSessionTemplate sqlSession;
-	
-	@Test
-	void contextLoads() {
-	}
-	
-	@Test
-	public void testSqlSessin() throws Exception{
-		System.out.println(sqlSession.toString());
-	}
 
 }
