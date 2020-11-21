4ca4da6cae6db9dfb89f9be9ac41f2ed41e53028
oryong2 <oryong2@EA-CHOI02.tsb.co.kr>
2020-11-21 17:26:25 +0900

    hook테스트1

--- src/main/java/board/BoardApplication.java
+++ src/main/java/board/BoardApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+ 
 @EnableJpaAuditing
 @EntityScan(
 		basePackageClasses = {Jsr310JpaConverters.class},
